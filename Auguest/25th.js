ed0e47be99018eb1c915b7fe2374d11f7cdad758
Johnson <Ashane199080706@hotmail.com.tw>
2023-08-25 11:48:01 -0700

    206. ReverseLinked List -easy

--- Auguest/25th.js
+++ Auguest/25th.js
@@ -11,3 +11,20 @@ function coverTitle(number) {
 
   return result;
 }
+
+// 206. reverse Linked List
+
+function reverseLinkedList(head) {
+  let prev = null;
+  let current = head;
+
+  while (current) {
+    const next = current.next; // set next to replace it
+    current.next = prev; // change the linked list point direction
+
+    prev = current;
+    current = next;
+  }
+
+  return prev;
+}
