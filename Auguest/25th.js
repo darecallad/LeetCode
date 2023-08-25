3d6967f9feac2f3c92b1864e9a2f0deec5797b63
Johnson <Ashane199080706@hotmail.com.tw>
2023-08-25 11:33:59 -0700

    168. Excel Sheet Column Title - easy

---
+++ Auguest/25th.js
@@ -0,0 +1,13 @@
+// 168. Excel Sheet Column Title
+
+function coverTitle(number) {
+  let result = "";
+
+  while (number > 0) {
+    let reminder = (number - 1) % 26; // excel is 1-based;
+    result = String.fromCharCode(65 + reminder) + result;
+    number = Math.floor((number - 1) / 26);
+  }
+
+  return result;
+}
