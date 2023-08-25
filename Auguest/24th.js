bf6012b7dfa2b2387c53f75beb9903ba4eccf1e6
Johnson <Ashane199080706@hotmail.com.tw>
2023-08-24 18:49:38 -0700

    283. Move Zeros - easy

--- Auguest/24th.js
+++ Auguest/24th.js
@@ -18,12 +18,22 @@ function romanToInt(s) {
     const currentValue = romanValue[s[i]];
     const nextValue = romanValue[s[i + 1]];
 
-    if (nextValue && nextValue > currentValue)
-      result =
-        result -
-        currentValue; // if next value is grater than current value, we - current value;
+    if (nextValue && nextValue > currentValue) result = result - currentValue;
+    // if next value is grater than current value, we - current value;
     else result = result + currentValue;
   }
 
   return result;
+}
+
+// 283. Move Zeros
+
+function moveZeros(nums) {
+  // noneZero index
+  let noneZero = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) nums[noneZero++] = nums[i]; // move none zero element to the correct position
+  }
+  for (let i = noneZero; i < nums.length; i++) nums[i] = 0; //fill the remaining positions with zeros
 }
