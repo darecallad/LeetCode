1c24e08fb75b76379bc95ea21464ae6e7c820d2a
Johnson <Ashane199080706@hotmail.com.tw>
2023-08-24 19:26:30 -0700

    69. Sqrt(x) - easy

--- Auguest/24th.js
+++ Auguest/24th.js
@@ -55,3 +55,23 @@ function searchInserPosition(nums, target) {
 
   return left;
 }
+
+// 69. Sqrt(x)
+
+function sqrt(x) {
+  if (x === 0) return 0;
+
+  let left = 1;
+  let right = Math.floor(x / 2) + 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const sqr = mid * mid;
+
+    if (sqr === x) return mid;
+    else if (sqr < x) left = mid + 1;
+    else right = mid - 1;
+  }
+
+  return right; // if the space root not found return the value of right;
+}
