8e01ce776330fb22ce2ab3eeeef18cc5f55cabee
Johnson <Ashane199080706@hotmail.com.tw>
2023-08-27 22:45:14 -0700

    2798. Number of Employees Who Met the Target - easy

--- Auguest/27th.js
+++ Auguest/27th.js
@@ -28,3 +28,13 @@ function sameTree(p, q) {
     p.val === q.val && sameTree(p.left, q.left) && sameTree(p.right, q.right)
   );
 }
+
+// 2798. Number of Employees Who Met the Target
+
+function countEmployee(hours, target) {
+  let count = 0;
+  for (let i = 0; i < hours.length; i++) {
+    if (hours[i] >= target) count++;
+  }
+  return count;
+}
