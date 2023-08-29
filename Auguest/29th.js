d4b49a57dd540f1e35b02ee4c34cffe1465762ec
Johnson <Ashane199080706@hotmail.com.tw>
2023-08-29 10:24:03 -0700

    // 83. Remove Duplicates from Sorted List - easy

--- Auguest/29th.js
+++ Auguest/29th.js
@@ -50,3 +50,15 @@ function findMaxAverage(nums, k) {
 
   return maxSum / k;
 }
+
+// 83. Remove Duplicates from Sorted List
+
+function deleteDuplicates(head) {
+  let cur = head;
+
+  while (cur && cur.next) {
+    if (cur.val === cur.next.val) cur.next = cur.next.next;
+    else cur = cur.next;
+  }
+  return head;
+}
