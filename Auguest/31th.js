939b5d579b55820e3b82e178506b035834729268
Johnson <Ashane199080706@hotmail.com.tw>
2023-08-31 13:46:10 -0700

    // 81. Search in Rotated Sorted Array II -medium

--- Auguest/31th.js
+++ Auguest/31th.js
@@ -64,3 +64,25 @@ function heapDown(heap) {
     } else break;
   }
 }
+
+// 81. Search in Rotated Sorted Array II
+
+function search(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left >= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) return true;
+
+    // left part sorted
+    if (nums[left] < nums[mid]) {
+      if (target >= nums[left] && target < nums[mid]) right = mid - 1;
+      else left = mid + 1;
+    } else if (nums[left] > nums[mid]) {
+      if (nums[mid] < target && target <= nums[right]) left = mid + 1;
+      else right = mid - 1;
+    } else left++;
+  }
+  return false;
+}
