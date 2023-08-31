91e286fe4df6f8205c16dd82a7886f7e2728378d
Johnson <Ashane199080706@hotmail.com.tw>
2023-08-31 12:25:43 -0700

    Word Break - medium

--- Auguest/31th.js
+++ Auguest/31th.js
@@ -6,3 +6,20 @@ function reverseInteger(num) {
   const n = Number(String(Math.abs(num)).split("").reverse().join(""));
   return n > limit ? 0 : n * k;
 }
+
+// 139. Word Break
+
+function wordBreak(s, wordDict) {
+  const dp = new Array(s.length + 1).fill(fasle);
+  dp[0] = true;
+
+  for (let i = 1; i < s.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (dp[j] && wordDict.includes(s.substring(j, i))) {
+        dp[i] = true;
+        break;
+      }
+    }
+  }
+  return dp[s.length];
+}
