514b40d6a4c584b6c6075daf18340e08c90552b7
Johnson <Ashane199080706@hotmail.com.tw>
2023-09-01 10:44:25 -0700

    //189. Rotate Array - medium

--- Sep/1th.js
+++ Sep/1th.js
@@ -16,3 +16,20 @@ function longestPalidorme(s) {
   }
   return ans;
 }
+
+//189. Rotate Array
+
+function rotate(nums, k) {
+  k %= nums.length;
+  reverse(nums, 0, nums.length - 1);
+  reverse(nums, 0, k - 1);
+  reverse(nums, k, nums.length - 1);
+}
+
+function reverse(nums, start, end) {
+  while (start < end) {
+    [nums[start], nums[end]] = [nums[end], nums[start]];
+    start++;
+    end--;
+  }
+}
