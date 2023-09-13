8d6223edae1e796c9e35430f657c0bb18bddca52
Johnson <Ashane199080706@hotmail.com.tw>
2023-09-13 11:07:55 -0700

    92. Reverse Linked List II - med

--- Sep/13th.js
+++ Sep/13th.js
@@ -7,3 +7,28 @@ function CountingBit(n) {
   }
   return ans;
 }
+
+// 92. Reverse Linked List II
+function reverseLinked(head, left, right) {
+  if (!head || left === right) return head;
+
+  let dummy = new LinkList(0);
+  dummy.next = head;
+  let prev = dymmy;
+
+  for (let i = 0; i < left - 1; i++) {
+    prev = prev.next;
+  }
+
+  let current = prev.next;
+  let next = null;
+
+  for (let i = 0; i < right - left; i++) {
+    next = current.next;
+    current.next = next.next;
+    next.next = prev.next;
+    prev.next = next;
+  }
+
+  return dummy.next;
+}
