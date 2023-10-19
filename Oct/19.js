5728a47929da37a0036f22b75eae1024425e4a12
Johnson <Ashane199080706@hotmail.com.tw>
2023-10-19 12:02:33 -0700

    198

--- Oct/19.js
+++ Oct/19.js
@@ -24,3 +24,27 @@ const hammingWeight = (n) => {
   }
   return count;
 };
+
+// 198
+
+const rob = (nums) => {
+  let n = nums.length;
+
+  if (n === 0) return 0;
+  if (n === 1) return nums[0];
+  if (n === 2) return Math.max(nums[0], nums[1]);
+
+  // base case
+  const dp = Array(n);
+  dp[0] = nums[0];
+  dp[1] = Math.max(nums[0], nums[1]);
+
+  // loop the house
+  for (let i = 2; i < n; i++) {
+    dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+  }
+
+  return dp[n - 1];
+};
+
+// time space O(n) -> time : go tho all the houses | space : cuz dp Array
